2bd6fb252f9b7f1188312948e8fb431acd2e9b4d
Arjun S <54721arjun@gmail.com>
2021-11-29 12:54:28 +0530

    code organized

--- and.py
+++ and.py
@@ -1,27 +1,33 @@
 from utils.model import Perceptron
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from utils.all_utils import prepare_data, save_plot, save_model
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+def main(data,eta,epochs,filename,plotFileName):
 
-df = pd.DataFrame(AND)
+    df = pd.DataFrame(data)
 
 
-X,y = prepare_data(df)
+    X,y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    _ = model.total_loss() #_ is dummy variable, we are not using it
 
-_ = model.total_loss()
+    save_model(model,filename=filename)
+    save_plot(df,plotFileName, model)
 
-save_model(model,filename="and.model")
-save_plot(df,"and.png", model)
+#ctrl+] to indent multiple lines
+
+if __name__=='__main__': #entry point
+
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=AND,eta=ETA,epochs=EPOCHS,filename="and.model",plotFileName='and.png')
