2bea54ec98c51d50d324dfe794dce4079b13efb4
Arjun S <54721arjun@gmail.com>
2021-12-01 15:00:35 +0530

    logging added and progress bar done

--- or.py
+++ or.py
@@ -2,12 +2,25 @@ from utils.model import Perceptron
 import pandas as pd
 import numpy as np
 from utils.all_utils import prepare_data, save_plot, save_model
+import logging 
+import os
+
+logging_str = "[%(asctime)s:%(levelname)s:%(module)s] %(message)s"
+log_dir="logs"
+os.makedirs(log_dir, exist_ok=True)
+#%(asctime)s: =>Time in which code is executed 
+#%(levelname)s: => Whcih information are you getting ..Is it the debug information ..or error information..or exception information etc
+#%(module)s: =>Which module has raised the error information
+#%(message)s: =>What message to print..eg:if the training is started ...if there is any exceptions 
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode='a')
+#filemode='a'used to append all the logs after each run
+#basic configuration is set only in the main file. We are calling all the modules in here.
 
 def main(data,eta,epochs,filename,plotFileName):
     
 
     df = pd.DataFrame(data)
-
+    logging.info(f"This is the actual dataframe {df}")
 
     X,y = prepare_data(df)
 
@@ -31,4 +44,10 @@ if __name__=='__main__':
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
 
-    main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+    try:
+        logging.info(">>>>> Starting training >>>>>")
+        main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+        logging.info("<<<<< Training doen successfully <<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -5,10 +5,13 @@ import joblib
 import os
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+import logging
 
 plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
 
 def prepare_data(df):
+    #logging.info("") is way better than print statement because we get more idea about from which module the statement is coming and so on.
+    logging.info("Preparing the data by segrregtaing the dependent and indepenedent variables")
     """It is used to seperate dependent variables and independent features          
 
     Args:
@@ -24,6 +27,7 @@ def prepare_data(df):
     return X, y
 
 def save_model(model, filename):
+    logging.info("Saving the trained model")
     """It saves the trained model
 
     Args:
@@ -34,6 +38,7 @@ def save_model(model, filename):
     os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
     filePath = os.path.join(model_dir, filename) # path will be model/filename
     joblib.dump(model, filePath)
+    logging.info(f"Saved the trained model in {filePath}")
 
 def save_plot(df, file_name, model):
     """
@@ -43,42 +48,45 @@ def save_plot(df, file_name, model):
 
     """
     def _create_base_plot(df):
+        logging.info("Creating the base plot")
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
         plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
         plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
         figure = plt.gcf() # get current figure
         figure.set_size_inches(10, 8)
 
-  def _plot_decision_regions(X, y, classfier, resolution=0.02):
-    colors = ("red", "blue", "lightgreen", "gray", "cyan")
-    cmap = ListedColormap(colors[: len(np.unique(y))])
+    def _plot_decision_regions(X, y, classfier, resolution=0.02):
+        logging.info("plotting the decision regions")
+        colors = ("red", "blue", "lightgreen", "gray", "cyan")
+        cmap = ListedColormap(colors[: len(np.unique(y))])
 
-    X = X.values # as a array
-    x1 = X[:, 0] 
-    x2 = X[:, 1]
-    x1_min, x1_max = x1.min() -1 , x1.max() + 1
-    x2_min, x2_max = x2.min() -1 , x2.max() + 1  
+        X = X.values # as a array
+        x1 = X[:, 0] 
+        x2 = X[:, 1]
+        x1_min, x1_max = x1.min() -1 , x1.max() + 1
+        x2_min, x2_max = x2.min() -1 , x2.max() + 1  
 
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
-                           np.arange(x2_min, x2_max, resolution))
-    print(xx1)
-    print(xx1.ravel())
-    Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
+                            np.arange(x2_min, x2_max, resolution))
+        #print(xx1)
+        #print(xx1.ravel())
+        Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        Z = Z.reshape(xx1.shape)
+        plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
 
-    plt.plot()
+        plt.plot()
 
 
 
-  X, y = prepare_data(df)
+    X, y = prepare_data(df)
 
-  _create_base_plot(df)
-  _plot_decision_regions(X, y, model)
+    _create_base_plot(df)
+    _plot_decision_regions(X, y, model)
 
-  plot_dir = "plots"
-  os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  plotPath = os.path.join(plot_dir, file_name) # model/filename
-  plt.savefig(plotPath)
+    plot_dir = "plots"
+    os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
+    plotPath = os.path.join(plot_dir, file_name) # model/filename
+    plt.savefig(plotPath)
+    logging.info(f"Saving the plot at {plotPath}")
